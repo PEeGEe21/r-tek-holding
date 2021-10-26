1f47d727982b486535e3d32feca73b344985ea90
Praise Udeh <mailpraiseudeh@gmail.com>
2021-10-26 18:28:28 +0100

    another commit

--- inc/contact.php
+++ inc/contact.php
@@ -7,7 +7,7 @@
 $from = 'enquiries@rthgnigeria.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'mailpraiseudeh';
+$sendTo = 'mailpraiseudeh@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
