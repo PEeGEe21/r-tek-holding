81c8116a07e129fd9e57075ede7f88cadff9d1fb
Praise Udeh <mailpraiseudeh@gmail.com>
2021-10-26 17:22:21 +0100

    another commit

--- inc/contact.php
+++ inc/contact.php
@@ -4,17 +4,17 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <iamelangodesigner@gmail.com>';
+$from = 'enquiries@rthgnigeria.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <iamelangodesigner@gmail.com>';
+$sendTo = 'mailpraiseudeh';
 
 // subject of the email
 $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'name', 'phone' => 'phone',  'email' => 'Email', 'message' => 'Message'); 
+$fields = array('name' => 'name', 'phone' => 'phone',  'email' => 'Email', 'company-name' => 'company-name', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
--- js/script.js
+++ js/script.js
@@ -204,10 +204,10 @@
                     items: 2
                 },
                 1024: {
-                    items: 2
+                    items: 3
                 },
                 1200: {
-                    items: 2
+                    items: 3
                 }
             }
         });
